1a349a212203313f890d2372e3e3817c238c08ca
uemotoakira <empty.aauu0001@gmail.com>
2017-07-10 00:04:54 +0900

    add kotlin(type4-7,9-10)

--- src/kotlin_part1/6_class.kt
+++ src/kotlin_part1/6_class.kt
@@ -7,15 +7,40 @@ package kotlin_part1
 // 自動的にコンストラクタを作成する
 class MyClass
 
+// クラスの定義とインスタンス化----------------------------------------------------------------
+// メソッド---------------------------------------------------------------------------------
+// プロパティ-------------------------------------------------------------------------------
+// this------------------------------------------------------------------------------------
+// コンストラクタとイニシャライズ--------------------------------------------------------------
+// エクステンション--------------------------------------------------------------------------
+
+
 fun main(args: Array<String>){
 
+    // クラスの定義とインスタンス化----------------------------------------------------------------
     // インスタンス化
     val myClass: MyClass = MyClass()
     println(myClass)
 
+    // メソッド---------------------------------------------------------------------------------
+    class Greeter{
+        fun greet(name: String){
+            println("Hello, $name!")
+        }
+    }
     val greeter = Greeter()
     greeter.greet("Kotlin") // Hello, Kotlin!
 
+    // プロパティ-------------------------------------------------------------------------------
+    class Person {
+        var name: String = ""
+        var age: Int = 0
+
+        // バッキングフィールド：必ず初期化が必要
+        val nameLength: Int
+            get() : Int {return this.name.length} // custom getter：対応プロパティが参照された時に呼び出されr、値を返す。
+
+    }
     var hanako = Person()
     println(hanako.name)
     println(hanako.age)
@@ -25,11 +50,33 @@ fun main(args: Array<String>){
     println(hanako.age)  // 25
     println(hanako.nameLength) //6
 
+    // パッキングフィールド
+    class Person2 {
+        var name: String = ""
+            set(value) {
+                println("${value}がセットされました")
+                // fieldはパッキングフィールドを表す暗黙変数
+                field = value // name = value とするとカスタムセッターが無限に呼び出される
+            }
+        var age: Int = 0
+        val nameLength: Int
+            get() = this.name.length // custom getter：対応プロパティが参照された時に呼び出されr、値を返す。
+    }
     var tom = Person2()
     tom.name = "tom"
     println(tom.name)
 
-    // コンストラクタとイニシャライザ
+
+    // コンストラクタとイニシャライズ--------------------------------------------------------------
+    // constructorを利用する
+    class Rational constructor(n: Int, d:Int){
+        val numerator: Int = n
+        val denominator: Int = d
+    }
+
+    // 引数にvalやvarを用いてインスタンス化する
+    class RationalR(val numerator: Int, val denominator:Int)
+
     val half = Rational(1,2)
     println(half.numerator) // 1
     println(half.denominator) // 2
@@ -39,87 +86,45 @@ fun main(args: Array<String>){
     println(halfR.denominator) // 2
 
     // セカンダリコンストラクタ
+    // プライマリコンストラクタ：numerator
+    // セカンダリコンストラクタ：denominator
+    class RationalRR(val numerator: Int, val denominator: Int){
+        // セカンダリコンストラクタの引数リストの後に、:記号を挟んで、プライマリコンストラクタを呼び出す
+        constructor(numerator: Int) : this(numerator, 1)
+    }
     val halfRR = RationalRR(1,2)
     println("${halfRR.numerator}/${halfRR.denominator}") // 1/2を出力
 
     val halfRR2 = RationalRR(5)
     println("${halfRR2.numerator}/${halfRR2.denominator}") // 5/1を出力
 
-
     // イニシャライザ
+    class RationalS(val numerator: Int, val denominator: Int = 1){
+        init{ require(denominator != 0 ) }
+    }
+    RationalS(1) //
+
+    // こっちの方が楽かな（とりあえず、初期化）
+    class RationalRR2(val numerator: Int, val denominator: Int = 1)
     RationalRR2(1,1)
     // RationalRR2(1,0) error
 
+    // エクステンション--------------------------------------------------------------------------
     // エクステンション(拡張関数)
     countWords("I like Kotlin") // 2（通常）
     "I like Kotlin".contWords() // 2（拡張関数）
-    println("I like Kotlin".wordsCount) // 2(拡張プロパティ)
+    println(" I like Kotlin ".wordsCount) // 2(拡張プロパティ)
 
 }
 
-class Greeter{
-    fun greet(name: String){
-        println("Hello, $name!")
-    }
-}
 
-class Person {
-    var name: String = ""
-    var age: Int = 0
-
-    // バッキングフィールド：必ず初期化が必要
-    val nameLength: Int
-    get() : Int {return this.name.length} // custom getter：対応プロパティが参照された時に呼び出されr、値を返す。
-
-}
-
-class Person2 {
-    var name: String = ""
-        set(value) {
-            println("${value}がセットされました")
-            // fieldはパッキングフィールドを表す暗黙変数
-            field = value // name = value とするとカスタムセッターが無限に呼び出される
-        }
-    var age: Int = 0
-    val nameLength: Int
-        get() = this.name.length // custom getter：対応プロパティが参照された時に呼び出されr、値を返す。
-}
-
+// プロパティ-------------------------------------------------------------------------------
 // 遅い初期化
 class MyClass2{
     lateinit var foo: String
 }
 
-// コンストラクタとイニシャライザ
-// constructorを利用する
-class Rational constructor(n: Int, d:Int){
-    val numerator: Int = n
-    val denominator: Int = d
-}
-
-// 引数にvalやvarを用いてインスタンス化する
-class RationalR(val numerator: Int, val denominator:Int)
-
-
-// セカンダリコンストラクタ
-// プライマリコンストラクタ：numerator
-// セカンダリコンストラクタ：denominator
-class RationalRR(val numerator: Int, val denominator: Int){
-    // セカンダリコンストラクタの引数リストの後に、:記号を挟んで、プライマリコンストラクタを呼び出す
-    constructor(numerator: Int) : this(numerator, 1)
-}
-
-// こっちの方が楽かな
-class RationalRR2(val numerator: Int, val denominator: Int = 1)
-
-
-// イニシャライザ
-class RationalS(val numerator: Int, val denominator: Int = 1){
-    init{
-        require(denominator != 0 )
-    }
-}
-
+// エクステンション--------------------------------------------------------------------------
 // エクステンション（拡張関数）：パッキングフィールドを持てない
 // 通常の関数：空白文字で分割して得られたリストの要素数を返す
 fun countWords(s: String): Int = s.split("""\s+"""".toRegex()).size
