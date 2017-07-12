3238900efff9b1aaf71482176168ccc3601a26f7
uemotoakira <empty.aauu0001@gmail.com>
2017-07-13 00:05:40 +0900

    add Readme変更

--- src/part1/Fortune/Fourtune.java
+++ src/part1/Fortune/Fourtune.java
@@ -7,18 +7,23 @@ public class Fourtune {
 
 	public static void main(String[] args) {
 
+		// 運勢開始
 		System.out.println("あなたの運勢を占います");
 		int fortune = new java.util.Random().nextInt(4);
 
-		if(fortune == 1) {
-			System.out.println("大吉");
-		} else if(fortune == 2) {
-			System.out.println(" 中吉");
-		} else if(fortune == 3) {
-			System.out.println("小吉");
-		} else {
-			System.out.println("吉");
-		}
+		// 運勢結果
+		String result = fortune ==1 ?  "大吉": fortune ==2 ?  "中吉": fortune ==3 ?  "小吉": "吉";
+		System.out.println(result);
+
+//		if(fortune == 1) {
+//			System.out.println("大吉");
+//		} else if(fortune == 2) {
+//			System.out.println(" 中吉");
+//		} else if(fortune == 3) {
+//			System.out.println("小吉");
+//		} else {
+//			System.out.println("吉");
+//		}
 
 	}
 }
