65573ba43080040623a510f4bbff126c6e36776e
uemotoakira <empty.aauu0001@gmail.com>
2017-08-06 23:18:06 +0900

    簡単な修正

--- src/part1/MultiplicationTable/Character.java
+++ src/part1/MultiplicationTable/Character.java
@@ -1,14 +1,32 @@
 package part1.MultiplicationTable;
 
+import java.util.*;
+
 public class Character {
 
     public static void main(String args[]){
         String[] characters = {"A","B","C","A","D","E","F","A"};
 
+        System.out.println("重複した文字を削除する前");
         for (String cha : characters) System.out.print(cha +" ");
-        String[] newChar;
 
+        System.out.println( "\n" + "---------------------");
+        System.out.println("\n" + "\n" + "重複した文字を削除する後 (hashset)");
+        Set chaSet = new HashSet<String>();
+        for (String cha : characters) chaSet.add(cha);
+        for (Iterator chaIte = chaSet.iterator(); chaIte.hasNext(); ) System.out.print(chaIte.next() +" ");
 
-        System.out.println("---------------------");
+        System.out.println( "\n" + "---------------------");
+        System.out.println("\n" + "\n" + "重複した文字を削除する後 (hashset以外)");
+        List<String> chaArray = new ArrayList<>();
+        for (String cha : characters) if(!chaArray.contains(cha)) chaArray.add(cha);
+        for (String cha : chaArray) System.out.print(cha + " ");
+
+        System.out.println( "\n" + "---------------------");
+        System.out.println("\n" +  "重複した文字を削除する後(stream)");
+        Arrays.stream(characters)
+                .distinct()
+                .forEach(cha -> System.out.print(cha + " "));
+
     }
 }
--- src/part1/MultiplicationTable/MulitplicationTable1.java
+++ src/part1/MultiplicationTable/MulitplicationTable1.java
@@ -10,27 +10,16 @@ public class MulitplicationTable1 {
         List<Integer> left = Arrays.asList(1,2,3,4,5,6,7,8,9);
         List<Integer> right = Arrays.asList(1,2,3,4,5,6,7,8,9);
 
+        System.out.println("----九九----");
         for(int l : left) {
-
             System.out.print("["+ l +"]" + "の段：");
-            for(int r : right) {
-                System.out.print(l*r + " ");
-            }
+            for(int r : right) System.out.print(l*r + " ");
             System.out.println();
         }
 
         System.out.println("---------------------------------");
 
-        left.forEach(l -> {
-            System.out.print("[" + l + "]" + "の段：");
-            for(int r : right) {
-                System.out.print(l*r + " ");
-            }
-            System.out.println();
-        });
-
-        System.out.println("---------------------------------");
-
+        System.out.println("----九九(ラムダ)----");
         left.forEach(l -> {
             System.out.print("[" + l + "]" + "の段：");
             right.forEach(r -> System.out.print(l*r + " "));
