65f1ece99fadff7c535be7a1137bc443fa228731
Uemoto Akira <a_uemoto@interspace.ne.jp>
2018-04-17 17:43:12 +0900

    画面変更

--- angular-tour-of-heroes/src/app/messages/messages.component.ts
+++ angular-tour-of-heroes/src/app/messages/messages.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core'
-import { MessageService } from '../service/message.service'
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../service/message.service';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.scss']
 })
 export class MessagesComponent implements OnInit {
 
